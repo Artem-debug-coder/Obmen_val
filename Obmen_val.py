92233e43889529fa2fd341740070340dde28f800
temid <temidizn@gmail.com>
2025-08-03 01:19:35 +0300

    Сощдано главное окно программы

---
+++ Obmen_val.py
@@ -0,0 +1,21 @@
+import requests
+import json
+from tkinter import *
+from tkinter import messagebox as mb
+
+
+
+
+
+window = Tk()
+window.title('Курсы обмена валют')
+window.geometry('360x80')
+
+Label(text='Введите код валюты').pack(padx=10, pady=10)
+
+entry = Entry()
+entry.pack(pady=10, padx=10)
+
+Button(text='Получить курс обмена к доллару', command=exchange).pack(pady=10, padx=10)
+
+window.mainloop()
